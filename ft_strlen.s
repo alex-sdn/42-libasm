4c21334d177f8b6b9e181867902d102196ad23bf
alex-sdn <alex@Alex.localdomain>
2024-06-26 14:57:36 +0200

    update syntax

--- ft_strlen.s
+++ ft_strlen.s
@@ -2,12 +2,12 @@ section .text
     global ft_strlen
 
 ft_strlen:
-    xor rax, rax            ; clears rax, same as "mov rax,0" but bit faster
-.loop:
+    xor rax, rax            ; clears rax
+loop:
     cmp byte [rdi + rax], 0 ; cmp the byte with 0
-    je  .return             ; jump if cmp was equal
+    je  end                 ; jump if cmp was equal
     inc rax                 ; rax++
-    jmp .loop               ; loop
+    jmp loop                ; loop
 
-.return:
-    ret
+end:
+    ret                     ; returns rax
