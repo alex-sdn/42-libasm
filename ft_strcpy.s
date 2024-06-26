c514f62069e22468137a6d3a4b5589ae06325777
alex-sdn <alex@Alex.localdomain>
2024-06-26 15:00:52 +0200

    update syntax

--- ft_strcpy.s
+++ ft_strcpy.s
@@ -1,16 +1,16 @@
 section .text
-    global ft_strcpy ; (*dest = rdi, *src = rsi)
+    global ft_strcpy    ; (*dest = rdi, *src = rsi)
 
 ft_strcpy:
     xor rcx, rcx        ; clear count register
-.loop:
+loop:
     mov al, [rsi + rcx] ; move char of src in al
     mov [rdi + rcx], al ; move al in dest
     cmp al, 0           ; compare char with null byte
-    je  .end            ; if equal -> over
+    je  end             ; if equal -> over
     inc rcx             ; rcx++
-    jmp .loop
+    jmp loop
 
-.end:
-    mov rax, rdi    ; returns address of dest string
+end:
+    mov rax, rdi        ; returns address of dest string
     ret
