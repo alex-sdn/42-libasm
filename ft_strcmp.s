f87a41539884b357cd5eefb317f2f438ce47c4bc
alex-sdn <alex@Alex.localdomain>
2024-06-26 14:59:40 +0200

    update syntax

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -3,28 +3,28 @@ section .text
 
 ft_strcmp:
     xor rcx, rcx        ; set count register at 0
-.loop:
+loop:
     mov al, [rdi + rcx] ; set current char in al
     mov bl, [rsi + rcx] ; _
     cmp al, bl          ; compare chars
-    jne .not_equal      ; jump if not equal
+    jne not_equal       ; jump if not equal
     cmp al, 0           ; if equal and null byte -> over
-    je  .equal
+    je  equal
     inc rcx             ; rcx++
-    jmp .loop
+    jmp loop
 
-.not_equal:
+not_equal:
     sub     al, bl      ; sub chars
     test    al, al      ; to return only 1 or -1
-    js      .negative   ; jump if signed
+    js      negative    ; jump if signed
     
     mov     rax, 1
     ret
 
-.negative:
+negative:
     mov     rax, -1
     ret
 
-.equal:
+equal:
     xor rax, rax        ; if equal return 0
     ret
