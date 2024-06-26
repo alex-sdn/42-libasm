b814b0dbe14c43c32abe6df228628423bb05e0ce
alex-sdn <alex@Alex.localdomain>
2024-06-26 14:54:38 +0200

    update comments

--- ft_read.s
+++ ft_read.s
@@ -6,7 +6,7 @@ ft_read:
     mov     rax, 0      ; move sys_read to rax
     syscall             ; syscall read()
     cmp     rax, 0
-    jge     .no_error   ; if rax >= 0 no errors
+    jge     no_error    ; if rax >= 0 no errors
 
     neg     rax                        ; rax *= -1
     push    rax                        ; push value in rax to stack
@@ -16,5 +16,5 @@ ft_read:
     mov     [rbx], rax                 ; store error code in errno address
     mov     rax, -1                    ; set return value to -1
 
-.no_error:
+no_error:
     ret                 ; returns value in rax
